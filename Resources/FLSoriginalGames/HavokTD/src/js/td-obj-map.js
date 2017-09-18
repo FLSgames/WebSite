ea946395c3df7eda45b20a74cb461c9b7de22768
FLSgames <31150149+FLSgames@users.noreply.github.com>
2017-09-18 04:26:58 -0500

    09/18/17
    This is the first release of 'Havok TD' By: FLS games. Removed all un-needed files. Clean up day!

---
+++ Resources/FLSoriginalGames/HavokTD/src/js/td-obj-map.js
@@ -0,0 +1,384 @@
+﻿/*
+ * ____ ____ ____ ____ ____ ____ ____ ____ 
+ *||F |||L |||S |||G |||A |||M |||E |||S ||
+ *||__|||__|||__|||__|||__|||__|||__|||__||
+ *|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|Copyright (c) 2017.
+ * 
+ *
+ * Author: FLS <DanielH@FlashLightningstudios.com>
+ * Website: http://www.FLSgames.com
+ *
+ * Latest Update: 09/18/2017
+ *
+ */
+
+// _TD.a.push begin
+_TD.a.push(function (TD) {
+
+	var _default_wait_clearInvalidElements = 20;
+
+	// map The properties and methods of the object. Note that there are no arrays, objects, etc. in the attribute
+	// Reference property, or the related properties of multiple instances conflict
+	var map_obj = {
+		_init: function (cfg) {
+			cfg = cfg || {};
+			this.grid_x = cfg.grid_x || 10;
+			this.grid_y = cfg.grid_y || 10;
+			this.x = cfg.x || 0;
+			this.y = cfg.y || 0;
+			this.width = this.grid_x * TD.grid_size;
+			this.height = this.grid_y * TD.grid_size;
+			this.x2 = this.x + this.width;
+			this.y2 = this.y + this.height;
+			this.grids = [];
+			this.entrance = this.exit = null;
+			this.buildings = [];
+			this.monsters = [];
+			this.bullets = [];
+			this.scene = cfg.scene;
+			this.is_main_map = !!cfg.is_main_map;
+			this.select_hl = TD.MapSelectHighLight(this.id + "-hl", {
+				map: this
+			});
+			this.select_hl.addToScene(this.scene, 1, 9);
+			this.selected_building = null;
+			this._wait_clearInvalidElements = _default_wait_clearInvalidElements;
+			this._wait_add_monsters = 0;
+			this._wait_add_monsters_arr = [];
+			if (this.is_main_map) {
+				this.mmm = new MainMapMask(this.id + "-mmm", {
+					map: this
+				});
+				this.mmm.addToScene(this.scene, 1, 7);
+
+			}
+
+			// Add the appropriate grid below
+			var i, l = this.grid_x * this.grid_y,
+				grid_data = cfg["grid_data"] || [],
+				d, grid;
+
+			for (i = 0; i < l; i++) {
+				d = grid_data[i] || {};
+				d.mx = i % this.grid_x;
+				d.my = Math.floor(i / this.grid_x);
+				d.map = this;
+				d.step_level = this.step_level;
+				d.render_level = this.render_level;
+				grid = new TD.Grid(this.id + "-grid-" + d.mx + "-" + d.my, d);
+				this.grids.push(grid);
+			}
+
+			if (cfg.entrance && cfg.exit && !TD.lang.arrayEqual(cfg.entrance, cfg.exit)) {
+				this.entrance = this.getGrid(cfg.entrance[0], cfg.entrance[1]);
+				this.entrance.is_entrance = true;
+				this.exit = this.getGrid(cfg.exit[0], cfg.exit[1]);
+				this.exit.is_exit = true;
+			}
+
+			var _this = this;
+			if (cfg.grids_cfg) {
+				TD.lang.each(cfg.grids_cfg, function (obj) {
+					var grid = _this.getGrid(obj.pos[0], obj.pos[1]);
+					if (!grid) return;
+					if (!isNaN(obj.passable_flag))
+						grid.passable_flag = obj.passable_flag;
+					if (!isNaN(obj.build_flag))
+						grid.build_flag = obj.build_flag;
+					if (obj.building) {
+						grid.addBuilding(obj.building);
+					}
+				});
+			}
+		},
+
+		/**
+		 * Check the map for weapons (aggressive buildings)
+		 * Because the first monster only appears when it has the first weapon on the map.
+		 */
+		checkHasWeapon: function () {
+			this.has_weapon = (this.anyBuilding(function (obj) {
+				return obj.is_weapon;
+			}) != null);
+		},
+
+		/**
+		 * Gets a lattice object at the specified location
+		 * @param mx {Number} Coordinates on the map x
+		 * @param my {Number} Coordinates on the map y
+		 */
+		getGrid: function (mx, my) {
+			var p = my * this.grid_x + mx;
+			return this.grids[p];
+		},
+
+		anyMonster: function (f) {
+			return TD.lang.any(this.monsters, f);
+		},
+		anyBuilding: function (f) {
+			return TD.lang.any(this.buildings, f);
+		},
+		anyBullet: function (f) {
+			return TD.lang.any(this.bullets, f);
+		},
+		eachBuilding: function (f) {
+			TD.lang.each(this.buildings, f);
+		},
+		eachMonster: function (f) {
+			TD.lang.each(this.monsters, f);
+		},
+		eachBullet: function (f) {
+			TD.lang.each(this.bullets, f);
+		},
+
+		/**
+		 * Pre-construction
+		 * @param building_type {String}
+		 */
+		preBuild: function (building_type) {
+			TD.mode = "build";
+			if (this.pre_building) {
+				this.pre_building.remove();
+			}
+
+			this.pre_building = new TD.Building(this.id + "-" + "pre-building-" + TD.lang.rndStr(), {
+				type: building_type,
+				map: this,
+				is_pre_building: true
+			});
+			this.scene.addElement(this.pre_building, 1, this.render_level + 1);
+			//this.show_all_ranges = true;
+		},
+
+		/**
+		 * Exit Pre-construction status
+		 */
+		cancelPreBuild: function () {
+			TD.mode = "normal";
+			if (this.pre_building) {
+				this.pre_building.remove();
+			}
+			//this.show_all_ranges = false;
+		},
+
+		/**
+		 * Clear invalid elements on map
+		 */
+		clearInvalidElements: function () {
+			if (this._wait_clearInvalidElements > 0) {
+				this._wait_clearInvalidElements--;
+				return;
+			}
+			this._wait_clearInvalidElements = _default_wait_clearInvalidElements;
+
+			var a = [];
+			TD.lang.shift(this.buildings, function (obj) {
+				if (obj.is_valid)
+					a.push(obj);
+			});
+			this.buildings = a;
+
+			a = [];
+			TD.lang.shift(this.monsters, function (obj) {
+				if (obj.is_valid)
+					a.push(obj);
+			});
+			this.monsters = a;
+
+			a = [];
+			TD.lang.shift(this.bullets, function (obj) {
+				if (obj.is_valid)
+					a.push(obj);
+			});
+			this.bullets = a;
+		},
+
+		/**
+		 * Add a monster to the entrance of the map
+		 * @param monster can be numbers or monster Object
+		 */
+		addMonster: function (monster) {
+			if (!this.entrance) return;
+			if (typeof monster == "number") {
+				monster = new TD.Monster(null, {
+					idx: monster,
+					difficulty: TD.difficulty,
+					step_level: this.step_level,
+					render_level: this.render_level + 2
+				});
+			}
+			this.entrance.addMonster(monster);
+		},
+
+		/**
+		 * Add at the entrance to the map n A monster
+		 * @param n
+		 * @param monster
+		 */
+		addMonsters: function (n, monster) {
+			this._wait_add_monsters = n;
+			this._wait_add_monsters_objidx = monster;
+		},
+
+		/**
+		 * arr In the form of:
+		 *     [[1, 0], [2, 5], [3, 6], [10, 4]...]
+		 */
+		addMonsters2: function (arr) {
+			this._wait_add_monsters_arr = arr;
+		},
+
+		/**
+		 * Check if the map's specified grid is available
+		 * @param mx {Number}
+		 * @param my {Number}
+		 */
+		checkPassable: function (mx, my) {
+			var grid = this.getGrid(mx, my);
+			return (grid != null && grid.passable_flag == 1 && grid.build_flag != 2);
+		},
+
+		step: function () {
+			this.clearInvalidElements();
+
+			if (this._wait_add_monsters > 0) {
+				this.addMonster(this._wait_add_monsters_objidx);
+				this._wait_add_monsters--;
+			} else if (this._wait_add_monsters_arr.length > 0) {
+				var a = this._wait_add_monsters_arr.shift();
+				this.addMonsters(a[0], a[1]);
+			}
+		},
+
+		render: function () {
+			var ctx = TD.ctx;
+			ctx.strokeStyle = "#99a";
+			ctx.lineWidth = _TD.retina;
+			ctx.beginPath();
+			ctx.strokeRect(this.x + 0.5, this.y + 0.5, this.width, this.height);
+			ctx.closePath();
+			ctx.stroke();
+		},
+
+		/**
+		 * Mouse out of Map events
+		 */
+		onOut: function () {
+			if (this.is_main_map && this.pre_building)
+				this.pre_building.hide();
+		}
+	};
+
+	/**
+	 * @param id {String} Configuration Object
+	 * @param cfg {Object} Configuration Object
+	 *         At a minimum, you need to include the following:
+ 
+	 *         {
+	 *			 grid_x: ,Width (lattice)
+	 *			 grid_y: Height (lattice),
+	 *			 scene: Which scene belongs to?,
+	 *		 }
+	 */
+	TD.Map = function (id, cfg) {
+		// map Currently only need to monitor out events
+		// Although you only need to listen for out events, you also need to listen for enter because if
+		// No enter, out will never be triggered
+		cfg.on_events = ["enter", "out"];
+		var map = new TD.Element(id, cfg);
+		TD.lang.mix(map, map_obj);
+		map._init(cfg);
+
+		return map;
+	};
+
+
+	/**
+	 * Map selected element Highlight Border object
+	 */
+	var map_selecthl_obj = {
+		_init: function (cfg) {
+			this.map = cfg.map;
+			this.width = TD.grid_size + 2;
+			this.height = TD.grid_size + 2;
+			this.is_visiable = false;
+		},
+		show: function (grid) {
+			this.x = grid.x;
+			this.y = grid.y;
+			this.is_visiable = true;
+		},
+		render: function () {
+			var ctx = TD.ctx;
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = "#f93";
+			ctx.beginPath();
+			ctx.strokeRect(this.x, this.y, this.width - 1, this.height - 1);
+			ctx.closePath();
+			ctx.stroke();
+		}
+	};
+
+	/**
+	 * Highlight box with map selected
+	 * @param id {String} You need to include at least
+	 * @param cfg {Object} You need to include at least
+ 
+	 *      {
+	 *          map: map Object
+	 *      }
+	 */
+	TD.MapSelectHighLight = function (id, cfg) {
+		var map_selecthl = new TD.Element(id, cfg);
+		TD.lang.mix(map_selecthl, map_selecthl_obj);
+		map_selecthl._init(cfg);
+
+		return map_selecthl;
+	};
+
+
+	var mmm_obj = {
+		_init: function (cfg) {
+			this.map = cfg.map;
+
+			this.x1 = this.map.x;
+			this.y1 = this.map.y;
+			this.x2 = this.map.x2 + 1;
+			this.y2 = this.map.y2 + 1;
+			this.w = this.map.scene.stage.width;
+			this.h = this.map.scene.stage.height;
+			this.w2 = this.w - this.x2;
+			this.h2 = this.h - this.y2;
+		},
+		render: function () {
+			var ctx = TD.ctx;
+			/*ctx.clearRect(0, 0, this.x1, this.h);
+			 ctx.clearRect(0, 0, this.w, this.y1);
+			 ctx.clearRect(0, this.y2, this.w, this.h2);
+			 ctx.clearRect(this.x2, 0, this.w2, this.h2);*/
+
+			ctx.fillStyle = "#fff";
+			ctx.beginPath();
+			ctx.fillRect(0, 0, this.x1, this.h);
+			ctx.fillRect(0, 0, this.w, this.y1);
+			ctx.fillRect(0, this.y2, this.w, this.h2);
+			ctx.fillRect(this.x2, 0, this.w2, this.h);
+			ctx.closePath();
+			ctx.fill();
+
+		}
+	};
+
+	/**
+	 * A mask outside the main map used to cover the range beyond the map.
+	 */
+	function MainMapMask(id, cfg) {
+		var mmm = new TD.Element(id, cfg);
+		TD.lang.mix(mmm, mmm_obj);
+		mmm._init(cfg);
+
+		return mmm;
+	}
+
+}); // _TD.a.push end
+
---
+++ Resources/FLSoriginalGames/HavokTD/src/js/td-walk.js
@@ -0,0 +1,228 @@
+﻿/*
+ * ____ ____ ____ ____ ____ ____ ____ ____ 
+ *||F |||L |||S |||G |||A |||M |||E |||S ||
+ *||__|||__|||__|||__|||__|||__|||__|||__||
+ *|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|Copyright (c) 2017.
+ * 
+ *
+ * Author: FLS <DanielH@FlashLightningstudios.com>
+ * Website: http://www.FLSgames.com
+ *
+ * Latest Update: 09/18/2017
+ *
+ */
+
+// _TD.a.push begin
+_TD.a.push(function (TD) {
+
+	/**
+	 * Use the A * algorithm (Dijkstra algorithm?) To find the shortest route from (x1, y1) to (x2, y2)
+	 *
+	 */
+	TD.FindWay = function (w, h, x1, y1, x2, y2, f_passable) {
+		this.m = [];
+		this.w = w;
+		this.h = h;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.way = [];
+		this.len = this.w * this.h;
+		this.is_blocked = this.is_arrived = false;
+		this.fPassable = typeof f_passable == "function" ? f_passable : function () {
+			return true;
+		};
+
+		this._init();
+	};
+
+	TD.FindWay.prototype = {
+		_init: function () {
+			if (this.x1 == this.x2 && this.y1 == this.y2) {
+				//If the coordinates you entered are already at the end. 
+				this.is_arrived = true;
+				this.way = [
+					[this.x1, this.y1]
+				];
+				return;
+			}
+
+			for (var i = 0; i < this.len; i++)
+				this.m[i] = -2; // -2 means not explored,-1 means unreachable
+
+			this.x = this.x1;
+			this.y = this.y1;
+			this.distance = 0;
+			this.current = [
+				[this.x, this.y]
+			]; //The current one-step exploration of the lattice
+
+			this.setVal(this.x, this.y, 0);
+
+			while (this.next()) {
+			}
+		},
+		getVal: function (x, y) {
+			var p = y * this.w + x;
+			return p < this.len ? this.m[p] : -1;
+		},
+		setVal: function (x, y, v) {
+			var p = y * this.w + x;
+			if (p > this.len) return false;
+			this.m[p] = v;
+		},
+		/**
+		* The neighbor with the specified coordinates, that is, from the specified coordinates, within 1 steps can reach the lattice
+		* Currently returns the top, bottom, left, and right four neighbourhoods of the specified lattice
+		* @param x {Number}
+		* @param y {Number}
+		*/
+		getNeighborsOf: function (x, y) {
+			var nbs = [];
+			if (y > 0) nbs.push([x, y - 1]);
+			if (x < this.w - 1) nbs.push([x + 1, y]);
+			if (y < this.h - 1) nbs.push([x, y + 1]);
+			if (x > 0) nbs.push([x - 1, y]);
+
+			return nbs;
+		},
+		/**
+		 *Gets all the neighbors of the n grid that are currently reachable by one step
+		 */
+		getAllNeighbors: function () {
+			var nbs = [], nb1, i, c, l = this.current.length;
+			for (i = 0; i < l; i++) {
+				c = this.current[i];
+				nb1 = this.getNeighborsOf(c[0], c[1]);
+				nbs = nbs.concat(nb1);
+			}
+			return nbs;
+		},
+		/**
+		* Backward from the end, looking for the nearest path from the beginning to the end
+		* The implementation here is to find the lowest (and more than 0) grid from the current grid, starting from the end point,
+		* Until the beginning of the arrival.
+		* This implementation needs to be repeated to find the neighborhood, and sometimes the value of more than one lattice in the adjacent lattice is the lowest, then
+		* Randomly select one. Another way to do this is to add in the first traversal to each of the squares that have arrived
+		* A value that points to the lattice (parent lattice) of its coming.
+		*/
+		findWay: function () {
+			var x = this.x2,
+				y = this.y2,
+				nb, max_len = this.len,
+				nbs_len,
+				nbs, i, l, v, min_v = -1,
+				closest_nbs;
+
+			while ((x != this.x1 || y != this.y1) && min_v != 0 &&
+			this.way.length < max_len) {
+
+				this.way.unshift([x, y]);
+
+				nbs = this.getNeighborsOf(x, y);
+				nbs_len = nbs.length;
+				closest_nbs = [];
+
+				//Find the smallest V in the neighborhood
+				min_v = -1;
+				for (i = 0; i < nbs_len; i++) {
+					v = this.getVal(nbs[i][0], nbs[i][1]);
+					if (v < 0) continue;
+					if (min_v < 0 || min_v > v)
+						min_v = v;
+				}
+				//Find all the smallest neighbourhoods in the V
+				for (i = 0; i < nbs_len; i++) {
+					nb = nbs[i];
+					if (min_v == this.getVal(nb[0], nb[1])) {
+						closest_nbs.push(nb);
+					}
+				}
+
+				//Randomly select one from the smallest neighbor of the V as the current grid
+				l = closest_nbs.length;
+				i = l > 1 ? Math.floor(Math.random() * l) : 0;
+				nb = closest_nbs[i];
+
+				x = nb[0];
+				y = nb[1];
+			}
+		},
+		/**
+		 * Reach the end
+		 */
+		arrive: function () {
+			this.current = [];
+			this.is_arrived = true;
+
+			this.findWay();
+		},
+		/**
+		 * Roads are blocked.
+		 */
+		blocked: function () {
+			this.current = [];
+			this.is_blocked = true;
+		},
+		/**
+		* Next iteration
+		* @return {Boolean} If the return value is true, indicates that the endpoint is not reached and that the path
+		* Not blocked, can continue iteration;
+		*/
+		next: function () {
+			var neighbors = this.getAllNeighbors(), nb,
+				l = neighbors.length,
+				valid_neighbors = [],
+				x, y,
+				i, v;
+
+			this.distance++;
+
+			for (i = 0; i < l; i++) {
+				nb = neighbors[i];
+				x = nb[0];
+				y = nb[1];
+				if (this.getVal(x, y) != -2) continue; //Current lattice has been explored
+				//grid = this.map.getGrid(x, y);
+				//if (!grid) continue;
+
+				if (this.fPassable(x, y)) {
+					// Can be
+
+					/**
+					* From the beginning to the cost of the current lattice
+					* Here's simply to take a few steps from the beginning to the current grid as a waste.
+					* In a more complex situation, it may also be necessary to consider the different road costs,
+					* For example, wetlands are more expensive than plains. But the current version of the road is not so complicated,
+					* Don't consider it first.
+					 */
+					v = this.distance;
+
+					valid_neighbors.push(nb);
+				} else {
+					//Do not pass or have a building block
+					v = -1;
+				}
+
+				this.setVal(x, y, v);
+
+				if (x == this.x2 && y == this.y2) {
+					this.arrive();
+					return false;
+				}
+			}
+
+			if (valid_neighbors.length == 0) {
+				this.blocked();
+				return false
+			}
+			this.current = valid_neighbors;
+
+			return true;
+		}
+	};
+
+}); // _TD.a.push end
+
+
