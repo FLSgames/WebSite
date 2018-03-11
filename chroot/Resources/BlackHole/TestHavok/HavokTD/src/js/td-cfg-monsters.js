23256f103eff1fa0b28e63cb4d71e37a046d6da6
Daniel <DanielH@flashlightningstudios.com>
2018-03-11 10:59:03 -0500

    Created subdomain & assests

---
+++ chroot/Resources/BlackHole/TestHavok/HavokTD/src/js/td-cfg-monsters.js
@@ -0,0 +1,198 @@
+﻿/*
+ * ____ ____ ____ ____ ____ ____ ____ ____ 
+ *||F |||L |||S |||G |||A |||M |||E |||S ||
+ *||__|||__|||__|||__|||__|||__|||__|||__||
+ *|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|Copyright (c) 2017.
+ * 
+ *
+ * Author: FLS <DanielH@FlashLightningstudios.com>
+ * Website: http://www.FLSgames.com
+ *
+ * Latest Update: 09/18/2017
+ *
+ */
+
+// _TD.a.push begin
+_TD.a.push(function (TD) {
+
+	/**
+	 * Default Monster Rendering method
+	 */
+	function defaultMonsterRender() {
+		if (!this.is_valid || !this.grid) return;
+		var ctx = TD.ctx;
+
+		// Draw a circle to represent the monster
+		ctx.strokeStyle = "#000";
+		ctx.lineWidth = 1;
+		ctx.fillStyle = this.color;
+		ctx.beginPath();
+		ctx.arc(this.cx, this.cy, this.r, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+
+		// Draw the life of the monster
+		if (TD.show_monster_life) {
+			var s = Math.floor(TD.grid_size / 4),
+				l = s * 2 - 2 * _TD.retina;
+			ctx.fillStyle = "#000";
+			ctx.beginPath();
+			ctx.fillRect(this.cx - s, this.cy - this.r - 6, s * 2, 4 * _TD.retina);
+			ctx.closePath();
+			ctx.fillStyle = "#f00";
+			ctx.beginPath();
+			ctx.fillRect(this.cx - s + _TD.retina, this.cy - this.r - (6 - _TD.retina), this.life * l / this.life0, 2 * _TD.retina);
+			ctx.closePath();
+		}
+	}
+
+	/**
+	 * Get the default properties for monsters
+	 * @param [monster_idx] {Number} Types of Monsters
+	 * @return attributes {Object}
+	 */
+	TD.getDefaultMonsterAttributes = function (monster_idx) {
+
+		var monster_attributes = [
+			{
+				// idx: 0
+				name: "monster 1",
+				desc: "The weakest monster.",
+				speed: 3,
+				max_speed: 10,
+				life: 50,
+				damage: 1, // How much damage will be done when you reach the end (1 ~ 10)
+				shield: 0,
+				money: 5 // How much money is available to destroy this monster (optional)
+			},
+			{
+				// idx: 1
+				name: "monster 2",
+				desc: "Slightly stronger little Monsters",
+				speed: 6,
+				max_speed: 20,
+				life: 50,
+				damage: 2, // How much damage will be done when you reach the end (1 ~ 10)
+				shield: 1
+			},
+			{
+				// idx: 2
+				name: "monster speed",
+				desc: "The fast-moving little monsters.",
+				speed: 12,
+				max_speed: 30,
+				life: 50,
+				damage: 3, //How much damage will be done when you reach the end (1 ~ 10)
+				shield: 1
+			},
+			{
+				// idx: 3
+				name: "monster life",
+				desc: "A very strong little monster of life.",
+				speed: 5,
+				max_speed: 10,
+				life: 500,
+				damage: 3, // How much damage will be done when you reach the end (1 ~ 10)
+				shield: 1
+			},
+			{
+				// idx: 4
+				name: "monster shield",
+				desc: "A very strong defensive little freak.",
+				speed: 5,
+				max_speed: 10,
+				life: 50,
+				damage: 3, // How much damage will be done when you reach the end (1 ~ 10)
+				shield: 20
+			},
+			{
+				// idx: 5
+				name: "monster damage",
+				desc: "The little freak with the big damage.",
+				speed: 7,
+				max_speed: 14,
+				life: 50,
+				damage: 10, // How much damage will be done when you reach the end (1 ~ 10)
+ 				shield: 2
+			},
+			{
+				// idx: 6
+				name: "monster speed-life",
+				desc: "Monsters with high speed and life",
+				speed: 15,
+				max_speed: 30,
+				life: 100,
+				damage: 3, // How much damage will be done when you reach the end (1 ~ 10)
+ 				shield: 3
+			},
+			{
+				// idx: 7
+				name: "monster speed-2",
+				desc: "Fast Monsters.",
+				speed: 30,
+				max_speed: 40,
+				life: 30,
+				damage: 4, // How much damage will be done when you reach the end (1 ~ 10)
+ 				shield: 1
+			},
+			{
+				// idx: 8
+				name: "monster shield-life",
+				desc: "A monster with very strong defenses and high life values.",
+				speed: 3,
+				max_speed: 10,
+				life: 300,
+				damage: 5, // How much damage will be done when you reach the end (1 ~ 10) 
+				shield: 15
+			}
+		];
+
+		if (typeof monster_idx == "undefined") {
+			// If only one argument is passed, only the number of monsters defined (for TD. JS) is returned.
+			return monster_attributes.length;
+		}
+
+		var attr = monster_attributes[monster_idx] || monster_attributes[0],
+			attr2 = {};
+
+		TD.lang.mix(attr2, attr);
+		if (!attr2.render) {
+			// If you do not specify a rendering method for the current monster
+			attr2.render = defaultMonsterRender
+		}
+
+		return attr2;
+	};
+
+
+	/**
+	 * Generate a list of monsters,
+ 	 * Contains n monsters
+ 	 * The monster type is specified in the range and is random if unspecified
+ 	 */
+	TD.makeMonsters = function (n, range) {
+		var a = [], count = 0, i, c, d, r, l = TD.monster_type_count;
+		if (!range) {
+			range = [];
+			for (i = 0; i < l; i++) {
+				range.push(i);
+			}
+		}
+
+		while (count < n) {
+			d = n - count;
+			c = Math.min(
+				Math.floor(Math.random() * d) + 1,
+				3 //Monsters of the same type show up to 3 at a time, preventing a large number of high defense or high speed monsters in a particular wave.
+ 			);
+			r = Math.floor(Math.random() * l);
+			a.push([c, range[r]]);
+			count += c;
+		}
+
+		return a;
+	};
+
+
+}); // _TD.a.push end
---
+++ chroot/Resources/FlashApps/HavokTD/src/js/td-obj-building.js
@@ -0,0 +1,563 @@
+﻿/*
+ * ____ ____ ____ ____ ____ ____ ____ ____ 
+ *||F |||L |||S |||G |||A |||M |||E |||S ||
+ *||__|||__|||__|||__|||__|||__|||__|||__||
+ *|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|Copyright (c) 2017.
+ * 
+ *
+ * Author: FLS <DanielH@FlashLightningstudios.com>
+ * Website: http://www.FLSgames.com
+ *
+ * Latest Update: 09/18/2017
+ *
+ */
+
+// _TD.a.push begin
+_TD.a.push(function (TD) {
+
+	// The properties and methods of the building object. Note that there are no arrays, objects, etc. in the attribute
+ 	// Reference property, or the related properties of multiple instances conflict
+ 	var building_obj = {
+		_init: function (cfg) {
+			this.is_selected = false;
+			this.level = 0;
+			this.killed = 0; // How many monsters are killed by the current building?
+ 			this.target = null;
+
+			cfg = cfg || {};
+			this.map = cfg.map || null;
+			this.grid = cfg.grid || null;
+
+			/**
+			 * Bullet type, can have the following types:
+			 * 1: Ordinary bullets
+			 * 2: Laser type, hit immediately after launch, not realized
+			 * 3: Missile class, hit will explode, bring face attack, temporarily not realized
+			 */
+			this.bullet_type = cfg.bullet_type || 1;
+
+			/**
+			 * Type possible values are:
+			 * "Wall": walls, no aggressive
+			 * "Cannon": Fort
+			 * "LMG": Light machine guns
+			 * "HMG": Heavy machine guns
+			 * "Laser_gun": Laser gun
+			 *
+			 */
+			this.type = cfg.type;
+
+			this.speed = cfg.speed;
+			this.bullet_speed = cfg.bullet_speed;
+			this.is_pre_building = !!cfg.is_pre_building;
+			this.blink = this.is_pre_building;
+			this.wait_blink = this._default_wait_blink = 20;
+			this.is_weapon = (this.type != "wall"); // Walls and other non-attack buildings this is false and the remaining weapons are true
+			var o = TD.getDefaultBuildingAttributes(this.type);
+			TD.lang.mix(this, o);
+			this.range_px = this.range * TD.grid_size;
+			this.money = this.cost; //Purchase and upgrade the cost of the building
+ 			this.caculatePos();
+		},
+
+		/**
+		 * Cost of upgrading the building
+		 */
+		getUpgradeCost: function () {
+			return Math.floor(this.money * 0.75);
+		},
+
+		/**
+		 * How much does it cost to sell the building?
+ 		 */
+		getSellMoney: function () {
+			return Math.floor(this.money * 0.5) || 1;
+		},
+
+		/**
+		 * Toggle Checked/Unchecked
+ 		 */
+		toggleSelected: function () {
+			this.is_selected = !this.is_selected;
+			this.grid.hightLight(this.is_selected); // Highlight
+ 			var _this = this;
+
+			if (this.is_selected) {
+				//If the current building is selected
+ 
+				this.map.eachBuilding(function (obj) {
+					obj.is_selected = obj == _this;
+				});
+				//Cancel the selected state of the selected building in another map
+ 				(
+					this.map.is_main_map ? this.scene.panel_map : this.scene.map
+				).eachBuilding(function (obj) {
+						obj.is_selected = false;
+						obj.grid.hightLight(false);
+					});
+				this.map.selected_building = this;
+
+				if (!this.map.is_main_map) {
+					// In the face map, the architecture is selected and the building mode is entered.
+ 					this.scene.map.preBuild(this.type);
+				} else {
+					// Cancel Building Mode 
+					this.scene.map.cancelPreBuild();
+				}
+
+			} else {
+				//If the current building is switched to unchecked
+ 
+				if (this.map.selected_building == this)
+					this.map.selected_building = null;
+
+				if (!this.map.is_main_map) {
+					// Cancel Building Mode
+ 					this.scene.map.cancelPreBuild();
+				}
+			}
+
+			// If you select/uncheck the building on the main map, show/hide the corresponding action button
+ 			if (this.map.is_main_map) {
+				if (this.map.selected_building) {
+					this.scene.panel.btn_upgrade.show();
+					this.scene.panel.btn_sell.show();
+					this.updateBtnDesc();
+				} else {
+					this.scene.panel.btn_upgrade.hide();
+					this.scene.panel.btn_sell.hide();
+				}
+			}
+		},
+
+		/**
+		 * Generate and update the description text for the upgrade button
+		 */
+		updateBtnDesc: function () {
+			this.scene.panel.btn_upgrade.desc = TD._t(
+				"upgrade", [
+					TD._t("building_name_" + this.type),
+					this.level + 1,
+					this.getUpgradeCost()
+				]);
+			this.scene.panel.btn_sell.desc = TD._t(
+				"sell", [
+					TD._t("building_name_" + this.type),
+					this.getSellMoney()
+				]);
+		},
+
+		/**
+		 * Put the building into a lattice
+		 * @param grid {Element} specify grid
+		 */
+		locate: function (grid) {
+			this.grid = grid;
+			this.map = grid.map;
+			this.cx = this.grid.cx;
+			this.cy = this.grid.cy;
+			this.x = this.grid.x;
+			this.y = this.grid.y;
+			this.x2 = this.grid.x2;
+			this.y2 = this.grid.y2;
+			this.width = this.grid.width;
+			this.height = this.grid.height;
+
+			this.px = this.x + 0.5;
+			this.py = this.y + 0.5;
+
+			this.wait_blink = this._default_wait_blink;
+			this._fire_wait = Math.floor(Math.max(2 / (this.speed * TD.global_speed), 1));
+			this._fire_wait2 = this._fire_wait;
+
+		},
+
+		/**
+		 * Remove the building completely
+		 */
+		remove: function () {
+//			TD.log("remove building #" + this.id + ".");
+			if (this.grid && this.grid.building && this.grid.building == this)
+				this.grid.building = null;
+			this.hide();
+			this.del();
+		},
+
+		/**
+		 * Looking for a target (monster)
+ 		 */
+		findTaget: function () {
+			if (!this.is_weapon || this.is_pre_building || !this.grid) return;
+
+			var cx = this.cx, cy = this.cy,
+				range2 = Math.pow(this.range_px, 2);
+
+			// If the current building has a goal, and the target is still valid, and the target is still within range
+ 			if (this.target && this.target.is_valid &&
+				Math.pow(this.target.cx - cx, 2) + Math.pow(this.target.cy - cy, 2) <= range2)
+				return;
+
+			// Looking for new targets in the range of monsters.
+ 			this.target = TD.lang.any(
+				TD.lang.rndSort(this.map.monsters), // Randomly sort the monsters
+ 				function (obj) {
+					return Math.pow(obj.cx - cx, 2) + Math.pow(obj.cy - cy, 2) <= range2;
+				});
+		},
+
+		/**
+		 * Gets the coordinates of the target (relative to the upper-left corner of the map)
+ 		 */
+		getTargetPosition: function () {
+			if (!this.target) {
+				// Targeting entrance
+ 				var grid = this.map.is_main_map ? this.map.entrance : this.grid;
+				return [grid.cx, grid.cy];
+			}
+			return [this.target.cx, this.target.cy];
+		},
+
+		/**
+		 * Fire on your target.
+ 		 */
+		fire: function () {
+			if (!this.target || !this.target.is_valid) return;
+
+			if (this.type == "laser_gun") {
+				// If it's a laser gun, the target is hit right away.
+				this.target.beHit(this, this.damage);
+				return;
+			}
+
+			var muzzle = this.muzzle || [this.cx, this.cy], // The position of the muzzle
+				cx = muzzle[0],
+				cy = muzzle[1];
+
+			new TD.Bullet(null, {
+				building: this,
+				damage: this.damage,
+				target: this.target,
+				speed: this.bullet_speed,
+				x: cx,
+				y: cy
+			});
+		},
+
+		tryToFire: function () {
+			if (!this.is_weapon || !this.target)
+				return;
+
+			this._fire_wait--;
+			if (this._fire_wait > 0) {
+//			return;
+			} else if (this._fire_wait < 0) {
+				this._fire_wait = this._fire_wait2;
+			} else {
+				this.fire();
+			}
+		},
+
+		_upgrade2: function (k) {
+			if (!this._upgrade_records[k])
+				this._upgrade_records[k] = this[k];
+			var v = this._upgrade_records[k],
+				mk = "max_" + k,
+				uk = "_upgrade_rule_" + k,
+				uf = this[uk] || TD.default_upgrade_rule;
+			if (!v || isNaN(v)) return;
+
+			v = uf(this.level, v);
+			if (this[mk] && !isNaN(this[mk]) && this[mk] < v)
+				v = this[mk];
+			this._upgrade_records[k] = v;
+			this[k] = Math.floor(v);
+		},
+
+		/**
+		 * Upgrade Building
+ 		 */
+		upgrade: function () {
+			if (!this._upgrade_records)
+				this._upgrade_records = {};
+
+			var attrs = [
+				// Upgradeable variables
+ 				"damage", "range", "speed", "life", "shield"
+			], i, l = attrs.length;
+			for (i = 0; i < l; i++)
+				this._upgrade2(attrs[i]);
+			this.level++;
+			this.range_px = this.range * TD.grid_size;
+		},
+
+		tryToUpgrade: function (btn) {
+			var cost = this.getUpgradeCost(),
+				msg = "";
+			if (cost > TD.money) {
+				msg = TD._t("not_enough_money", [cost]);
+			} else {
+				TD.money -= cost;
+				this.money += cost;
+				this.upgrade();
+				msg = TD._t("upgrade_success", [
+					TD._t("building_name_" + this.type), this.level,
+					this.getUpgradeCost()
+				]);
+			}
+
+			this.updateBtnDesc();
+			this.scene.panel.balloontip.msg(msg, btn);
+		},
+
+		tryToSell: function () {
+			if (!this.is_valid) return;
+
+			TD.money += this.getSellMoney();
+			this.grid.removeBuilding();
+			this.is_valid = false;
+			this.map.selected_building = null;
+			this.map.select_hl.hide();
+			this.map.checkHasWeapon();
+			this.scene.panel.btn_upgrade.hide();
+			this.scene.panel.btn_sell.hide();
+			this.scene.panel.balloontip.hide();
+		},
+
+		step: function () {
+			if (this.blink) {
+				this.wait_blink--;
+				if (this.wait_blink < -this._default_wait_blink)
+					this.wait_blink = this._default_wait_blink;
+			}
+
+			this.findTaget();
+			this.tryToFire();
+		},
+
+		render: function () {
+			if (!this.is_visiable || this.wait_blink < 0) return;
+
+			var ctx = TD.ctx;
+
+			TD.renderBuilding(this);
+
+			if (
+				this.map.is_main_map &&
+				(
+					this.is_selected || (this.is_pre_building) ||
+					this.map.show_all_ranges
+				) &&
+				this.is_weapon && this.range > 0 && this.grid
+			) {
+				// Draw Range
+				ctx.lineWidth = _TD.retina;
+				ctx.fillStyle = "rgba(255, 136, 136, 0.15)";
+				ctx.strokeStyle = "#C30101";
+				ctx.beginPath();
+				ctx.arc(this.cx, this.cy, this.range_px, 0, Math.PI * 2, true);
+				ctx.closePath();
+				ctx.fill();
+				ctx.stroke();
+			}
+
+			if (this.type == "laser_gun" && this.target && this.target.is_valid) {
+				// Painting laser
+				ctx.lineWidth = 3 * _TD.retina;
+				ctx.strokeStyle = "rgba(109, 255, 12, 0.5)";
+				ctx.beginPath();
+				ctx.moveTo(this.cx, this.cy);
+				ctx.lineTo(this.target.cx, this.target.cy);
+				ctx.closePath();
+				ctx.stroke();
+				ctx.lineWidth = _TD.retina;
+				ctx.strokeStyle = "rgba(150, 150, 255, 0.5)";
+				ctx.beginPath();
+				ctx.lineTo(this.cx, this.cy);
+				ctx.closePath();
+				ctx.stroke();
+			}
+		},
+
+		onEnter: function () {
+			if (this.is_pre_building) return;
+
+			var msg = "Building fortifications";
+			if (this.map.is_main_map) {
+				msg = TD._t("building_info" + (this.type == "wall" ? "_wall" : ""), [TD._t("building_name_" + this.type), this.level, this.damage, this.speed, this.range, this.killed]);
+			} else {
+				msg = TD._t("building_intro_" + this.type, [TD.getDefaultBuildingAttributes(this.type).cost]);
+			}
+
+			this.scene.panel.balloontip.msg(msg, this.grid);
+		},
+
+		onOut: function () {
+			if (this.scene.panel.balloontip.el == this.grid) {
+				this.scene.panel.balloontip.hide();
+			}
+		},
+
+		onClick: function () {
+			if (this.is_pre_building || this.scene.state != 1) return;
+			this.toggleSelected();
+		}
+	};
+
+	/**
+	 * @param id {String}
+	 * @param cfg {object} Configuration Object
+	 * 		Need to include at least the following:
+	 * 		{		
+	 * 			Type: Construction types, optional values have
+	 *				 "wall"
+	 *				 "cannon"
+	 *				 "LMG"
+	 *				 "HMG"
+	 *				 "laser_gun"
+	 *		 }
+	 */
+	TD.Building = function (id, cfg) {
+		cfg.on_events = ["enter", "out", "click"];
+		var building = new TD.Element(id, cfg);
+		TD.lang.mix(building, building_obj);
+		building._init(cfg);
+
+		return building;
+	};
+
+
+	// The properties and methods of the bullet object. Note that there are no arrays, objects, etc. in the attribute
+	// Reference property, or the related properties of multiple instances conflict
+ 	var bullet_obj = {
+		_init: function (cfg) {
+			cfg = cfg || {};
+
+			this.speed = cfg.speed;
+			this.damage = cfg.damage;
+			this.target = cfg.target;
+			this.cx = cfg.x;
+			this.cy = cfg.y;
+			this.r = cfg.r || Math.max(Math.log(this.damage), 2);
+			if (this.r < 1) this.r = 1;
+			if (this.r > 6) this.r = 6;
+
+			this.building = cfg.building || null;
+			this.map = cfg.map || this.building.map;
+			this.type = cfg.type || 1;
+			this.color = cfg.color || "#252424";
+
+			this.map.bullets.push(this);
+			this.addToScene(this.map.scene, 1, 6);
+
+			if (this.type == 1) {
+				this.caculate();
+			}
+		},
+
+		/**
+		 * Calculate the number of bullets
+ 		 */
+		caculate: function () {
+			var sx, sy, c,
+				tx = this.target.cx,
+				ty = this.target.cy,
+				speed;
+			sx = tx - this.cx;
+			sy = ty - this.cy;
+			c = Math.sqrt(Math.pow(sx, 2) + Math.pow(sy, 2));
+			speed = 20 * this.speed * TD.global_speed;
+			this.vx = sx * speed / c;
+			this.vy = sy * speed / c;
+		},
+
+		/**
+		 * Check if the current bullet is out of the map range
+ 		 */
+		checkOutOfMap: function () {
+			this.is_valid = !(
+				this.cx < this.map.x ||
+				this.cx > this.map.x2 ||
+				this.cy < this.map.y ||
+				this.cy > this.map.y2
+			);
+
+			return !this.is_valid;
+		},
+
+		/**
+		 * Check if the current bullet hit the monster.
+ 		 */
+		checkHit: function () {
+			var cx = this.cx,
+				cy = this.cy,
+				r = this.r * _TD.retina,
+				monster = this.map.anyMonster(function (obj) {
+					return Math.pow(obj.cx - cx, 2) + Math.pow(obj.cy - cy, 2) <= Math.pow(obj.r + r, 2) * 2;
+				});
+
+			if (monster) {
+				// Hit the monster
+ 				monster.beHit(this.building, this.damage);
+				this.is_valid = false;
+
+				// Bullet small explosion effect
+ 				TD.Explode(this.id + "-explode", {
+					cx: this.cx,
+					cy: this.cy,
+					r: this.r,
+					step_level: this.step_level,
+					render_level: this.render_level,
+					color: this.color,
+					scene: this.map.scene,
+					time: 0.2
+				});
+
+				return true;
+			}
+			return false;
+		},
+
+		step: function () {
+			if (this.checkOutOfMap() || this.checkHit()) return;
+
+			this.cx += this.vx;
+			this.cy += this.vy;
+		},
+
+		render: function () {
+			var ctx = TD.ctx;
+			ctx.fillStyle = this.color;
+			ctx.beginPath();
+			ctx.arc(this.cx, this.cy, this.r, 0, Math.PI * 2, true);
+			ctx.closePath();
+			ctx.fill();
+		}
+	};
+
+	/**
+	 *@param ID {String} Configuration Object
+	 * @param cfg {object} Configuration Object
+	 * 		Need to include at least the following:
+	 * 		{
+	 * 			x: Location of bullets
+	 * 			Y: Where the bullets are sent
+	 * 			Speed:
+	 * 			Damage:
+	 * 			Target: Goal, a Monster object
+	 * 			Building: The building to which it belongs
+	 *	}
+	 * Bullet type, can have the following types:
+	 * 1: Ordinary bullets
+	 * 2: Laser type, hit immediately after launch
+	 * 3: Missile class, hit will explode, bring face attack
+	 */
+	TD.Bullet = function (id, cfg) {
+		var bullet = new TD.Element(id, cfg);
+		TD.lang.mix(bullet, bullet_obj);
+		bullet._init(cfg);
+
+		return bullet;
+	};
+
+}); // _TD.a.push end
+
